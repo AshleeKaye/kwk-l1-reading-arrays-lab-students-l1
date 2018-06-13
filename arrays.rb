920ba91bfc8a3e55c78f8d4d237076390b56f8bc
AshleeKaye <ashleemk@hotmail.com>
2018-06-13 22:01:32 +0000

    automatically backed up by learn

---
+++ arrays.rb
@@ -0,0 +1,12 @@
+#student_1 = "Ashlee"
+#student_2 = "Alyssa"
+#student_3 = "Victoria"
+#student_4 = "Baily"
+student_names = ["Ashlee", "Alyssa"]
+#puts student_names
+student_names.push("Victoria")
+student_names.push("Baily")
+student_names.push("Meh")
+
+puts student_names.size
+puts student_names.length
